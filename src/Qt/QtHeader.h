f6bf5a2af44589cd3798644b07c9b1c52efe241f
Akira <jukaka@pku.edu.cn>
2019-12-31 13:42:14 +0800

    first step, pass building test on ubuntu16.04 with osg support

---
+++ src/Qt/QtHeader.h
@@ -0,0 +1,35 @@
+//
+// Created by akira on 19-12-30.
+//
+
+#ifndef OPENROADED_QTHEADER_H
+#define OPENROADED_QTHEADER_H
+#include <QWidget>
+#include <QtGui>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QToolButton>
+#include <QRadioButton>
+#include <QGroupBox>
+#include <QDoubleSpinBox>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QApplication>
+#include <QListWidget>
+#include <QComboBox>
+#include <QFormLayout>
+#include <QMainWindow>
+#include <QDialog>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextEdit>
+#include <QDockWidget>
+#include <QScrollArea>
+#include <QAction>
+#include <QMenu>
+#include <QToolBar>
+#include <QMenuBar>
+#include <QStatusBar>
+#endif //OPENROADED_QTHEADER_H
---
+++ src/Qt/ToolsDialogs/LoadingDialog.cpp
@@ -0,0 +1,30 @@
+
+#include "LoadingDialog.h"
+#include "moc_LoadingDialog.cpp"
+
+
+
+/**
+ * Constructor
+ * Initializes the widgets that make up the interface
+ */
+LoadingDialog::LoadingDialog()
+{
+	//-------------------------------------------------
+	QVBoxLayout *lDialogLayout = new QVBoxLayout;
+	//-------------------------------------------------
+	QLabel *lSoftware = new QLabel("Loading");
+	lSoftware->setFont(QFont("Arial",18,10));
+	lSoftware->setAlignment(Qt::AlignHCenter);
+	QLabel *lNames = new QLabel("Loading road structure and generating geometry");
+	lNames->setAlignment(Qt::AlignHCenter);
+	//-------------------------------------------------
+	lDialogLayout->addWidget(lSoftware);
+	lDialogLayout->addWidget(lNames);
+	//-------------------------------------------------
+	setLayout(lDialogLayout);
+	//-------------------------------------------------
+
+	setWindowFlags(Qt::Popup);
+	setWindowTitle("Loading");
+}
